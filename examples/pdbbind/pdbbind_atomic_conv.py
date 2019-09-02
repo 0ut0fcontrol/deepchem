9987f0d8e958a5f6e661f9a1afe3fe31fb184c27
jcyang <gold.yang@qq.com>
2019-09-02 10:09:31 +0800

    use pdbbind core set as test set

--- examples/pdbbind/pdbbind_atomic_conv.py
+++ examples/pdbbind/pdbbind_atomic_conv.py
@@ -14,12 +14,17 @@ import argparse
 import numpy as np
 import tensorflow as tf
 import deepchem as dc
+from datetime import datetime as dt
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("-max_epoch", type=int, default=100)
 parser.add_argument("-patience", type=int, default=3)
 parser.add_argument("-version", default='2015')
 parser.add_argument("-subset", default='core')
+parser.add_argument(
+    "-test",
+    help="test set, e.g. core. split test set from subset if not given.")
+parser.add_argument("-remove_ids", help="ids need to remove from training set")
 parser.add_argument("-component", default='binding')
 parser.add_argument("-split", default='random')
 parser.add_argument("-seed", type=int, default=111)
@@ -35,6 +40,8 @@ parser.add_argument("-timestamp", action='store_true')
 parser.add_argument("-split_complex", action='store_true')
 args = parser.parse_args()
 
+start = dt.now()
+
 # np seed for split only
 np.random.seed(args.seed)
 # tf seed not work, every training will different.
@@ -45,6 +52,29 @@ frag2_num_atoms = 1067  # for pocket atoms without Hs
 # frag2_num_atoms = 1350  # for pocket atoms with Hs
 # frag2_num_atoms = 24000  # for protein atoms
 complex_num_atoms = frag1_num_atoms + frag2_num_atoms
+
+if args.test is not None:
+  args.split = None
+  pdbbind_tasks, pdbbind_datasets, transformers = dc.molnet.load_pdbbind(
+      reload=args.reload,
+      featurizer="atomic",
+      version='2015',
+      frag1_num_atoms=frag1_num_atoms,
+      frag2_num_atoms=frag2_num_atoms,
+      split=args.split,
+      split_seed=args.seed,
+      clust_file=args.clust_file,
+      split_complex=args.split_complex,
+      same_protein=args.same_protein,
+      same_ligand=args.same_ligand,
+      subset='core',
+      load_binding_pocket=True,
+      data_dir=args.data_dir,
+      save_dir=args.save_dir,
+      save_timestamp=args.timestamp,
+      transform=args.trans,
+  )
+  test_dataset, _, _ = pdbbind_datasets
 
 pdbbind_tasks, pdbbind_datasets, transformers = dc.molnet.load_pdbbind(
     reload=args.reload,
@@ -69,7 +99,28 @@ pdbbind_tasks, pdbbind_datasets, transformers = dc.molnet.load_pdbbind(
 if args.feat_only:
   raise SystemExit(0)
 
-train_dataset, valid_dataset, test_dataset = pdbbind_datasets
+if args.test is not None:
+  dataset, _, _ = pdbbind_datasets
+  test_ids = set(test_dataset.ids)
+  keep_inds = []
+  dataset_ids = dataset.ids
+  for i, id_ in enumerate(dataset_ids):
+    if id_ in test_ids:
+      continue
+    else:
+      keep_inds.append(i)
+  print(f"keep {len(keep_inds)}/{len(dataset_ids)} in dataset")
+  N = len(keep_inds)
+  N_train = int(N * 0.9)
+  np.random.seed(args.seed)
+  perm = np.random.permutation(N)
+  keep_inds = np.array(keep_inds)[perm]
+  train_inds = sorted(keep_inds[:N_train])
+  valid_inds = sorted(keep_inds[N_train:])
+  train_dataset = dataset.select(train_inds)
+  valid_dataset = dataset.select(valid_inds)
+else:
+  train_dataset, valid_dataset, test_dataset = pdbbind_datasets
 
 metrics = [
     dc.metrics.Metric(dc.metrics.pearson_r2_score),
@@ -82,7 +133,7 @@ batch_size = 16
 
 # default
 # atom_types=[6, 7, 8, 9, 11, 12, 15, 16, 17, 20, 25, 30, 35, 53, -1]
-atom_types=[1, 6, 7, 8, 9, 12, 15, 16, 17, 20, 25, 30, 35, 53, -1]
+atom_types = [1, 6, 7, 8, 9, 12, 15, 16, 17, 20, 25, 30, 35, 53, -1]
 
 # [[Rc],[Rs], [Re]], Rc is cutoff, Rs is mean, Re is variance.
 default_radial = [[
@@ -90,7 +141,7 @@ default_radial = [[
     9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0
 ], [0.0, 4.0, 8.0], [0.4]]
 
-min_radial = [[1.0, 2.0, 3.0, 4.0, 5.0],[0.0, 2.0, 4.0],[0.4]]
+min_radial = [[1.0, 2.0, 3.0, 4.0, 5.0], [0.0, 2.0, 4.0], [0.4]]
 model = dc.models.AtomicConvModel(
     batch_size=batch_size,
     atom_types=atom_types,
@@ -147,7 +198,8 @@ for i in range(args.max_epoch):
   valid_scores = valid_evaluator.compute_model_performance(metrics)
   print("Validation scores")
   print(valid_scores)
-
+  if np.isnan(valid_scores['pearson_r2_score']):
+    break
   if valid_scores['pearson_r2_score'] < best_r2:
     patience += 1
     if patience > args.patience:
@@ -157,7 +209,11 @@ for i in range(args.max_epoch):
     best_checkpoint = copy_checkpoint(last_checkpoint)
     patience = 0
     best_r2 = valid_scores['pearson_r2_score']
-    print('### Better at epoch {}\n'.format(i + 1))
+    print('### Better on valid at epoch {}'.format(i + 1))
+    test_scores = test_evaluator.compute_model_performance(metrics)
+    print("Testing scores")
+    print(test_scores)
+    print()
 
 model.restore(checkpoint=best_checkpoint)
 train_scores = train_evaluator.compute_model_performance(
@@ -187,3 +243,10 @@ with open('splitted_ids.json', 'w') as f:
       'test': list(test_dataset.ids)
   }
   json.dump(data, f, indent=2)
+
+if args.test is not None:
+  import shutil
+  shutil.rmtree(train_dataset.data_dir)
+  shutil.rmtree(valid_dataset.data_dir)
+
+print(f"Elapsed time {dt.now()- start}")
