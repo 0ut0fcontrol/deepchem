f50a96696b1a4743b24c4e66b2742e174b847bdd
jcyang <gold.yang@qq.com>
2019-09-09 15:31:04 +0800

    clustering pdbbind using protein sequences

---
+++ examples/pdbbind/similarity/protein_sequence/README.md
@@ -0,0 +1,4 @@
+```
+python pdbbind2protein_simi.py -d path/to/v2015 -e ./usearch11.0.667_i86linux32 v2015_ids result/v2015
+python pdbbind2protein_simi.py -d path/to/v2018 -e ./usearch11.0.667_i86linux32 v2018_ids result/v2018
+```
--- examples/pdbbind/similarity/protein_sequence/pdbbind2protein_simi.py
+++ examples/pdbbind/similarity/protein_sequence/pdbbind2protein_simi.py
@@ -1,4 +1,4 @@
-"""computing protein sequence similarity for PDBbind using NWalign.
+"""convert PDBbind to fasta format, and then cluster by uclust in usearch.
 """
 import re
 import json
@@ -7,93 +7,103 @@ import subprocess
 import numpy as np
 from tqdm import tqdm
 from pathlib import Path
-from multiprocessing import Pool
 from datetime import datetime as dt
+
+from Bio import PDB
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from multiprocessing import Pool
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('index', help='PDBbind data index')
 parser.add_argument('output', help='output data in json')
 parser.add_argument(
-    '-d', '--data_dir', required=True, help='PDBbind data dir, like ./v2015')
+    '-d', '--data_dir', required=True, help="PDBbind data dir, like ./v2015")
 parser.add_argument(
-    '-e', '--exe', default='./NWalign', help='NWalign exe, default: ./NWalign')
+    '-e',
+    '--exe',
+    help='usearch exe, default: ./usearch11.0.667_i86linux32',
+    default='./usearch11.0.667_i86linux32')
 args = parser.parse_args()
+start = dt.now()
 
-data_dir = Path(args.data_dir)
+DATADIR = Path(args.data_dir)
+
+
+def SeqFromPDBCode(code):
+  protein_pdb = DATADIR / code / (code + '_protein.pdb')
+  parser = PDB.PDBParser(QUIET=True)
+  try:
+    protein = parser.get_structure(code, protein_pdb)
+  except:
+    print('fail to read {}'.format(code))
+    return code, None
+  ppb = PDB.PPBuilder()
+  seqs = []
+  for chain in protein.get_chains():
+    seqs.extend([i.get_sequence() for i in ppb.build_peptides(chain)])
+    seq_str = ''.join([str(i) for i in seqs])
+    a = seqs[0].alphabet
+    return code, Seq(seq_str, a)
+
 
 with open(args.index) as f:
   patt = re.compile(r'^(?P<pdb_id>\w{4}).*', re.MULTILINE)
-  index = np.array(re.findall(patt, f.read()))
-# use np.array and np.int32 for less memory and faster process fork in imap
-pairs = np.array(np.triu_indices(len(index), 1), dtype=np.int32).T
+  codes = np.array(re.findall(patt, f.read()))
 
-aligned_ident_pattern = re.compile(
-    r".*Aligned length:\s+(\d+)\nIdentical length:\s+(\d+).*")
-
-
-def NWalign(pair):
-  i, j = pair
-  id_i, id_j = index[i], index[j]
-  pdb_i = str(data_dir / id_i / (id_i + '_protein.pdb'))
-  pdb_j = str(data_dir / id_j / (id_j + '_protein.pdb'))
-  cmd = (args.exe, pdb_i, pdb_j, '1')
-  try:
-    job = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    log = job.stdout.decode('utf-8')
-    matchs = re.findall(aligned_ident_pattern, log)
-    aligned_len, ident_len = matchs[0]
-    identity = float(ident_len) / float(aligned_len)
-    return identity
-  except:
-    # print(cmd)
-    return 0.
-
-
-# use np.float16 for less memory
-simi_triu = np.zeros(len(pairs), dtype=np.float16)
+succeeded_codes = []
+seqs = []
+Nones = []
 p = Pool()
-jobs = tqdm(
-    p.imap(NWalign, pairs, chunksize=100),
-    desc='NWalign',
-    total=len(pairs),
-    smoothing=0,
-)
-
-for idx, identity in enumerate(jobs):
-  simi_triu[idx] = identity
+iter_seqs = p.imap(SeqFromPDBCode, codes)
+for code, seq in tqdm(iter_seqs, total=len(codes)):
+  if seq is not None:
+    succeeded_codes.append(code)
+    seqs.append(seq)
 p.close()
+print(f'succeeded {len(seqs)}/{len(codes)}')
 
-dist_triu = 1 - simi_triu
+fasta_file = args.index + '.fasta'
+with open(fasta_file, 'w') as f:
+  for code, seq in zip(succeeded_codes, seqs):
+    SeqIO.write(SeqRecord(seq, id=code, description=''), f, 'fasta')
+print(f'Sequences save at {fasta_file}')
 
-from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
-Z = linkage(dist_triu)
-fig = plt.figure(figsize=(25, 10))
-if len(index) < 1000:
-  dn = dendrogram(Z)
-  plt.savefig(args.output + '.png')
-
-with open(args.output + f".simi.json", 'w') as f:
-  result = {
-      'index': index.tolist(),
-      'simi_triu': simi_triu.tolist(),
-  }
-  json.dump(result, f)
-
-for simi_cutoff in (0.5, 0.6, 0.7, 0.8, 0.9):
-  dist_cutoff = 1 - simi_cutoff
-  cluster_mask = fcluster(Z, t=dist_cutoff, criterion='distance')
-  cluster_nums = set(cluster_mask)
+def uclust2clust(filename):
+  cluster_mask = []
+  codes = []
+  with open(filename) as f:
+    for line in f:
+      fields = line.split()
+      cluster_mask.append(int(fields[1]))
+      codes.append(fields[8])
+  cluster_num = set(cluster_mask)
+  cluster_mask = np.array(cluster_mask)
+  codes = np.array(codes)
   clusters = []
-  for num in cluster_nums:
-    clust = list(index[cluster_mask == num])
+  for num in cluster_num:
+    clust = list(codes[cluster_mask == num])
     clusters.append(clust)
-  json_name = args.output + f'.clust.ident{simi_cutoff:.1f}.json'
+  return clusters
+
+
+output_names = []
+for simi_cutoff in (0.4, 0.5, 0.6, 0.7, 0.8, 0.9):
+  uclust_file = f'{args.output}.simi{simi_cutoff}.uc'
+  subprocess.check_call([
+      args.exe, '-cluster_fast', fasta_file, '-id',
+      str(simi_cutoff), '-uc', uclust_file
+  ])
+  clusters = uclust2clust(uclust_file)
+  json_name = f'{args.output}.simi{simi_cutoff:.1f}.clust.json'
+  output_names.append(json_name)
   with open(json_name, 'w') as f:
-    result = {
-        'clusters': clusters,
-    }
-    json.dump(result, f, indent=4)
+    json.dump(clusters, f, indent=4)
+
+print("clusters result in files:")
+for i in output_names:
+  print(i)
+print(f'Elapsed time {dt.now() - start}.')
